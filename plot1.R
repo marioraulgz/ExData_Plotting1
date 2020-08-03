8a8c4eab4d822ed8ec4dfeb63cba630891172c36
Mario Raul <64337574+marioraulgz@users.noreply.github.com>
2020-08-03 02:19:51 -0500

    Added comments

--- plot1.R
+++ plot1.R
@@ -2,6 +2,7 @@ library(lubridate)
 library(dplyr)
 library(tidyr)
 
+#Get data and subset it with a pipeline
 hpc <- read.csv("household_power_consumption.txt", sep = ";")
 hpc <- hpc %>% filter(Date=="1/2/2007" | Date =="2/2/2007") %>% unite("Date Time", Date:Time , sep =" ") %>% mutate(`Date Time`= dmy_hms(`Date Time`))
 #1
